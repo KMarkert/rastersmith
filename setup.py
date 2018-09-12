4d7c4f16dce535dc10bfd3e3b103077542dac455
kmarkert <kel.markert@gmail.com>
2018-09-11 23:47:00 -0500

    added setup files for installation

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='rastersmith',
+      version='0.0.1',
+      description='RasterSmith is a package to preprocess different NASA Earth observing satellite data products into common resolution, spatial reference, and format for easy analysis and processing across sensors.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://github.com/kmarkert/rastersmith',
+      packages=setuptools.find_packages(),
+      author='Kel Markert',
+      author_email='kel.markert@gmail.com',
+      license='MIT',
+      zip_safe=False)
